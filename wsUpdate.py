5b54ef7a73f8718c7fa2c8437214013841e84abe
Sandeep Bharadwaj <sandeepbl@gmail.com>
2017-01-10 15:36:03 -0500

    Partial success - only one to one communication

--- wsUpdate.py
+++ wsUpdate.py
@@ -1,8 +1,24 @@
-from sys import stdout,argv
-inputStr = ''
+import sys
+import time,json
+
+# Comment from here to ----------------------------->
+# from datetime import datetime
+# import threading
+# def send_time():
+	# while True:
+		# time_data = '{"type":"time","time":"' + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + '"}'
+		# print time_data
+		# sys.stdout.flush()
+		# time.sleep(1)
+# sendtimer = threading.Thread(target=send_time)	
+# sendtimer.start()
+# here to remove sending time stamp to websocket <---
+
 while True:
-	try:
-		inputStr = input()
-	except EOFError:
-		print inputStr,str(argv[1])
-		stdout.flush()
+	inputstream = sys.stdin.readline()
+	sys.stdin.flush()
+	if str(inputstream) != '':
+		inputstream_json =  json.loads(sys.stdin.readline())
+		for element in inputstream_json:
+			print element,inputstream_json[element]
+		sys.stdout.flush()
